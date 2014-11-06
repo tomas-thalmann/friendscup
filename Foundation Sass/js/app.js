8b72b16faa33d2cb5bd0a3d017ed5dba85484c31
pesrepus <sebi.thaler@gmail.com>
2014-11-06 08:39:06 +0100

    Cup Page Morphing Finished
    - added enquire.js for better media query handling in javascript
    - added jquery.modal.js for morphing on cup page (css top handling
    failed on foundation modal)
    - cleared up app.js with strict mode and global definitions
    - cup page: improvements on html and scss

--- Foundation Sass/js/app.js
+++ Foundation Sass/js/app.js
@@ -2,6 +2,8 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 // $(document).foundation();
 
+/*global $, jQuery, enquire, Foundation*/
+
 // ---------------------------
 // Foundation
 // ---------------------------
@@ -13,102 +15,40 @@ $(document).foundation({
         mobile_show_parent_link: false
     },
     reveal : {
-        animation: 'fadeAndPop',
-        animation_speed: 200,
-        css : {
-            open : {
-                'opacity': 0,
-                'visibility': 'visible',
-                'display': 'block'
-            },
-            close : {
-                'opacity': 1,
-                'visibility': 'hidden',
-                'display': 'none'
-            }
-        }
+        animation: 'fade',
+        animation_speed: 200
     }
 });
 
 // ---------------------------
-// Fittext
+// Other Custom
 // ---------------------------
 
-$( document ).ready(function() {
-    if (matchMedia) {
-        var mq = window.matchMedia(Foundation.media_queries['medium']);
-        mq.addListener(WidthChange);
-        WidthChange(mq);
-    }
-
-    function WidthChange(mq) {
-        if (mq.matches) {
-            
-            // ----- MEDIUM UP -----
-            $(".big-button h1").fitText(0.7, { minFontSize: '32px', maxFontSize: '44px' }); // Standard Foundation h1: 44px
-            $(".cup-header-left h3").fitText(1, { minFontSize: '10px', maxFontSize: '27px' }); // Standard Foundation h1: 44px
-            
+$(document).ready(function () {
+    "use strict";
+    enquire.register(Foundation.media_queries.medium, {
+        match: function () {
+            // ----- Fittext -----
+            // Standard Foundation h1: 44px
+            $(".big-button h1").fitText(0.7, { minFontSize: '32px', maxFontSize: '44px' });
+            // Standard Foundation h3: 27px
+            $(".cup-header-left h3").fitText(1, { minFontSize: '10px', maxFontSize: '27px' });
         }
-        else {
-            
-            // ----- SMALL -----
-            $(".big-button h1").fitText(0.7, { minFontSize: '22px', maxFontSize: '34px' }); // Standard Foundation h1 at small: 34px
-            
-        }
-    }
-});
-
-// ---------------------------
-// Expanding
-// ---------------------------
-
-$.fn.clicktoggle = function(a, b) {
-    return this.each(function() {
-        var clicked = false;
-        $(this).click(function() {
-            if (clicked) {
-                clicked = false;
-                return b.apply(this, arguments);
-            }
-            clicked = true;
-            return a.apply(this, arguments);
-        });
     });
-};
-
-$( document ).ready(function() {
-    $(".ch-expanded").hide();
-    $(".black-overlay").hide();
-    
-    $(".section-header-expand").clicktoggle(function() {
-        $(".ch-compressed").fadeOut(200);
-        $(".black-overlay").fadeIn(400);
-        $(".cp-section").delay(200).queue(function(){
-            $(this).addClass("cp-section-expanded").dequeue();
-        });
-        $(".ch-expanded").delay(400).fadeIn(200);
-    },function() {
-        $(".ch-expanded").fadeOut(200);
-        $(".black-overlay").fadeOut(400);
-        $(".cp-section").delay(200).queue(function(){
-            $(this).removeClass("cp-section-expanded").dequeue();
-        });
-        $(".ch-compressed").delay(400).fadeIn(200);
+    enquire.register(Foundation.media_queries.small, {
+        match: function () {
+            // ----- Fittext -----
+            // Standard Foundation h1 at small: 34px
+            $(".big-button h1").fitText(0.7, { minFontSize: '22px', maxFontSize: '34px' });
+        }
     });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
+$('.ch-section .modal-open').click(function () {
+    "use strict";
+    $('.ch-section .section-modal').css('top', $('.ch-section').offset().top);
+    $('.ch-section .section-modal').modal();
+});
 
 
 
