7ee4183a188c327f6009e82d3af11a3d61805e70
pesrepus <sebi.thaler@gmail.com>
2015-01-08 21:26:35 +0100

    One Page Explanation First Image
    Added the image for the first point

--- Foundation Sass/config.rb
+++ Foundation Sass/config.rb
@@ -17,6 +17,9 @@ javascripts_dir = "js"
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
+# To disable warnings. Uncomment:
+disable_warnings = true
+
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
@@ -24,11 +27,24 @@ javascripts_dir = "js"
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 
+# -----------------
+# Autoprefixer
+# -----------------
+
 require 'autoprefixer-rails'
 
 on_stylesheet_saved do |file|
   css = File.read(file)
-  File.open(file, 'w') do |io|
-    io << AutoprefixerRails.process(css)
+  map = file + '.map'
+
+  if File.exists? map
+    result = AutoprefixerRails.process(css,
+      from: file,
+      to:   file,
+      map:  { prev: map, inline: false })
+    File.open(file, 'w') { |io| io << result.css }
+    File.open(map,  'w') { |io| io << result.map }
+  else
+    File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
   end
 end
